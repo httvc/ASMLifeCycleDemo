38a6bce252cad6dac3303482bc2d9284e741e584
httvc <923351919@qq.com>
2020-06-22 22:52:33 +0800

    提交插件化

--- app/src/main/java/com/httvc/asmlifecycledemo/PluginManager.java
+++ app/src/main/java/com/httvc/asmlifecycledemo/PluginManager.java
@@ -15,15 +15,20 @@ package com.httvc.asmlifecycledemo;
     }
 }*/
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.IntentFilter;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.res.AssetManager;
 import android.content.res.Resources;
 
 import java.io.File;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.List;
 
 import dalvik.system.DexClassLoader;
 
@@ -72,6 +77,58 @@ public class  PluginManager{
         } catch (InvocationTargetException e) {
             e.printStackTrace();
         }
+
+        parseReceivers(context,path);
+    }
+
+    //解析插件静态广播
+    private void parseReceivers(Context context, String path) {
+        try {
+            Class<?> packageParserClass = Class.forName("android.content.pm.PackageParser");
+            Method parsePackageMethod = packageParserClass.getDeclaredMethod("parsePackage", File.class, int.class);
+            Object packageParser = packageParserClass.newInstance();
+            Object packageObj=parsePackageMethod.invoke(packageParser,new File(path),PackageManager.GET_ACTIVITIES);
+
+            Field receiversField=packageObj.getClass().getDeclaredField("receivers");
+            List receivers=(List)receiversField.get(packageObj);
+
+            Class<?> componentClass = Class.forName("android.content.pm.PackageParser$Component");
+            Field intentsField = componentClass.getDeclaredField("intents");
+
+            Class<?> packageParser$ActivityClass = Class.forName("android.content.pm.PackageParser$Activity");
+            Class<?> packageUserStateClass = Class.forName("android.content.pm.PackageUserState");
+            Object defaltUserState = packageUserStateClass.newInstance();
+
+            Method generateReceiverInfo = packageParserClass.getDeclaredMethod("generateActivityInfo", packageParser$ActivityClass, int.class, packageUserStateClass, int.class);
+
+            Class<?> userHandler = Class.forName("android.os.UserHandle");
+            Method getCallingUserId = userHandler.getDeclaredMethod("getCallingUserId");
+            int userId = (int)getCallingUserId.invoke(null);
+
+            for(Object activity:receivers){
+                ActivityInfo info=(ActivityInfo) generateReceiverInfo.invoke(packageParser,activity,0,defaltUserState,userId);
+                List<? extends IntentFilter> intentFilters = (List<? extends IntentFilter>) intentsField.get(activity);
+                BroadcastReceiver broadcastReceiver = (BroadcastReceiver) dexClassLoader.loadClass(info.name).newInstance();
+
+                for (IntentFilter intentFilter:intentFilters){
+                    context.registerReceiver(broadcastReceiver,intentFilter);
+                }
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
     public Resources getResources(){
